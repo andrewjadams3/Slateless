b8c15a46b8a4581c1faa1bb0b79d3f4df7b0d036
Andrew Adams <andrewjadams3@gmail.com>
2014-07-20 14:15:08 -0500

    Initial commit

---
+++ public/js/draw.js
@@ -0,0 +1,42 @@
+var scheme   = "ws://";
+var uri      = scheme + window.document.location.host + "/";
+var ws       = new WebSocket(uri);
+
+// The minimum distance the mouse has to drag
+// before firing the next onMouseDrag event:
+tool.minDistance = 1;
+
+var path;
+
+function onMouseDown(event) {
+  startPath(event.point["x"], event.point["y"]);
+  ws.send(JSON.stringify({ message: "start", x: event.point["x"], y: event.point["y"] }));
+}
+
+function onMouseDrag(event) {
+  drawPath(event.point["x"], event.point["y"]);
+  ws.send(JSON.stringify({ message: "draw", x: event.point["x"], y: event.point["y"] }));
+}
+
+function startPath(x, y) {
+  // console.log("Drawing... X: " + x + " Y: " + y)
+  path = new Path();
+  path.strokeColor = '#00000';
+  path.add(x, y)
+}
+
+function drawPath(x, y) {
+  // console.log("Drawing... X: " + x + " Y: " + y)
+  path.add(x, y)
+}
+
+ws.onmessage = function(message) {
+  var data = JSON.parse(message.data);
+  if (data.message == "start") {
+    startPath(data.x, data.y);
+    paper.view.draw();
+  } else {
+    drawPath(data.x, data.y);
+    paper.view.draw();
+  }
+};
