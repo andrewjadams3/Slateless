4cfed5d067004a2a5c9c1211d13a06f4336abca5
Andrew Adams <andrewjadams3@gmail.com>
2014-07-21 18:33:50 -0500

    Add basic toolbar CSS & HTML

--- public/js/draw.js
+++ public/js/draw.js
@@ -1,3 +1,4 @@
+// Websocket setup
 var scheme = "ws://";
 var loc = window.location, new_uri;
 if (loc.protocol === "https:") {
@@ -8,7 +9,7 @@ if (loc.protocol === "https:") {
 uri += "//" + loc.host + loc.pathname;
 var ws = new WebSocket(uri);
 
-//
+// Circle cursor in canvas
 var circlePath = new Path.Circle(new Point(10, 10), 2.5);
 circlePath.strokeColor = 'black'
 
@@ -18,10 +19,11 @@ function onMouseMove(event) {
 
 // The minimum distance the mouse has to drag
 // before firing the next onMouseDrag event:
-tool.minDistance = 2;
+tool.minDistance = 1;
 
-var path;
+var path; // Set the path
 
+// Mouse events
 function onMouseDown(event) {
   startPath(event.point["x"], event.point["y"]);
   ws.send(JSON.stringify({ message: "start", x: event.point["x"], y: event.point["y"] }));
@@ -34,32 +36,38 @@ function onMouseDrag(event) {
 }
 
 function onMouseUp(event) {
-  // When the mouse is released, simplify it:
-  path.simplify();
+  // When the mouse is released, simplify the path:
+  simplify();
+  ws.send(JSON.stringify({ message: "simplify" }));
 }
 
+
+// Drawing functions
 function startPath(x, y) {
-  // console.log("Drawing... X: " + x + " Y: " + y)
   path = new Path();
-  path.strokeColor = '#00000';
-  path.strokeWidth = 5;
-  path.strokeCap = 'round';
-  path.strokeJoin = 'round';
+  path.style = style;
   path.add(x, y)
 }
 
 function drawPath(x, y) {
-  // console.log("Drawing... X: " + x + " Y: " + y)
   path.add(x, y)
 }
 
+function simplify() {
+  path.simplify();
+}
+
+// Receiving socket messages
 ws.onmessage = function(message) {
   var data = JSON.parse(message.data);
   if (data.message == "start") {
     startPath(data.x, data.y);
     paper.view.draw();
-  } else {
+  } else if (data.message == "draw") {
     drawPath(data.x, data.y);
+    paper.view.draw();
+  } else {
+    simplify();
     paper.view.draw();
   }
 };
---
+++ public/js/options.js
@@ -0,0 +1,7 @@
+// Stroke style
+var style = {
+  strokeColor: '#000000',
+  strokeWidth: 5,
+  strokeCap: 'round',
+  strokeJoin: 'round'
+}
