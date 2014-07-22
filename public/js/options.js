d1a188e243d883417248f9eaa0f765f5856057a3
Andrew Adams <andrewjadams3@gmail.com>
2014-07-21 21:15:07 -0500

    Add style capability to websockets

--- public/js/options.js
+++ public/js/options.js
@@ -1,12 +1,13 @@
 // Stroke style
 var style = {
   strokeColor: '#000000',
-  strokeWidth: 5,
+  strokeWidth: 10,
   strokeCap: 'round',
   strokeJoin: 'round'
 }
 
 $(document).on('ready', function() {
+  // Colors
   $("#black").click(function() {style.strokeColor = '#000000'});
   $("#red").click(function() {style.strokeColor = '#FF0000'});
   $("#green").click(function() {style.strokeColor = '#009900'});
@@ -15,4 +16,9 @@ $(document).on('ready', function() {
   $("#orange").click(function() {style.strokeColor = '#FF9900'});
   $("#brown").click(function() {style.strokeColor = '#802A2A'});
   $("#purple").click(function() {style.strokeColor = '#663399'});
+
+  // Width
+  $("#small").click(function() {style.strokeWidth: 2});
+  $("#medium").click(function() {style.strokeWidth: 5});
+  $("#large").click(function() {style.strokeWidth: 10});
 });
