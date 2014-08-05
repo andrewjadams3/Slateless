33753cf6e76f66b3aba457fa02d83f39b31df66b
Andrew Adams <andrewjadams3@gmail.com>
2014-08-05 08:02:47 -0500

    encapsulate websocket creation

--- public/js/draw.js
+++ public/js/draw.js
@@ -1,19 +1,16 @@
 // Websocket setup
-var scheme = "ws://";
-var loc = window.location, new_uri;
-if (loc.protocol === "https:") {
-  uri = "wss:";
-} else {
-  uri = "ws:";
+function newSocket() {
+  var uri;
+  var scheme = "ws://";
+  var loc = window.location, new_uri;
+  if (loc.protocol === "https:") {
+    uri = "wss:";
+  } else {
+    uri = "ws:";
+  };
+  uri += "//" + loc.host + loc.pathname;
+  return (new WebSocket(uri));
 }
-uri += "//" + loc.host + loc.pathname;
-var ws = new WebSocket(uri);
-
-// Receiving socket messages
-ws.onmessage = function(event) {
-  var message = JSON.parse(event.data);
-  DrawApp[message.type](message.path);
-};
 
 // Tool
 tool.minDistance = 1;
@@ -31,15 +28,16 @@ PathAttributes.prototype.updatePoint = function(point) {
   this.y = point.y;
 }
 
-PathAttributes.prototype.sendMessage = function(type) {
-  ws.send(JSON.stringify({type: type, path: this}));
+PathAttributes.prototype.sendMessage = function(type, socket) {
+  socket.send(JSON.stringify({type: type, path: this}));
 };
 
 var DrawApp = {
   paths: {},
   path_order: [],
   currentPathAttrs: null,
-  cursor: new Path.Circle(new Point(-10, -10), 2.5)
+  cursor: new Path.Circle(new Point(-10, -10), 2.5),
+  ws: newSocket()
 };
 
 DrawApp.cursor.strokeColor = 'black';
@@ -63,7 +61,7 @@ DrawApp.simplify = function(attr) {
 };
 
 DrawApp.request = function() {
-  ws.send(JSON.stringify({ type: "redraw", path: DrawApp.paths }));
+  this.ws.send(JSON.stringify({ type: "redraw", path: DrawApp.paths }));
 };
 
 DrawApp.redraw = function(path_array) {
@@ -81,22 +79,27 @@ function onMouseMove(event) {
 
 function onMouseDown(event) {
   DrawApp.currentPathAttrs = new PathAttributes(event.point);
-  DrawApp.currentPathAttrs.sendMessage("startPath");
+  DrawApp.currentPathAttrs.sendMessage("startPath", DrawApp.ws);
   DrawApp.startPath(DrawApp.currentPathAttrs);
 }
 
 function onMouseDrag(event) {
   DrawApp.cursor.position = event.point;
   DrawApp.currentPathAttrs.updatePoint(event.point);
-  DrawApp.currentPathAttrs.sendMessage("drawPath");
+  DrawApp.currentPathAttrs.sendMessage("drawPath", DrawApp.ws);
   DrawApp.drawPath(DrawApp.currentPathAttrs);
 }
 
 function onMouseUp() {
   DrawApp.simplify(DrawApp.currentPathAttrs);
-  DrawApp.currentPathAttrs.sendMessage("simplify");
+  DrawApp.currentPathAttrs.sendMessage("simplify", DrawApp.ws);
 }
 
+// Receiving socket messages
+DrawApp.ws.onmessage = function(event) {
+  var message = JSON.parse(event.data);
+  DrawApp[message.type](message.path);
+};
 
 // Globals
 window.DrawGlobals = {};
