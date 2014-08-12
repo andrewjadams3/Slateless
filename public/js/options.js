ce9caf2034eb46e7ba4b1b94a450a878534b8de4
Andrew Adams <andrewjadams3@gmail.com>
2014-08-11 23:10:22 -0500

    Implement reset, clean up modals

--- public/js/options.js
+++ public/js/options.js
@@ -8,24 +8,46 @@ var style = {
 
 $(document).on('ready', function() {
   // Colors
-  $("#black").click(function() {style.strokeColor = '#000000'});
-  $("#red").click(function() {style.strokeColor = '#FF0000'});
-  $("#green").click(function() {style.strokeColor = '#009900'});
-  $("#blue").click(function() {style.strokeColor = '#000099'});
-  $("#yellow").click(function() {style.strokeColor = '#FFFF00'});
-  $("#orange").click(function() {style.strokeColor = '#FF9900'});
-  $("#brown").click(function() {style.strokeColor = '#802A2A'});
-  $("#purple").click(function() {style.strokeColor = '#663399'});
-
+  $("#black").click(function(event) {selectColor(event, '#colorsModal', '#000000')});
+  $("#red").click(function(event) {selectColor(event, '#colorsModal', '#FF0000')});
+  $("#green").click(function(event) {selectColor(event, '#colorsModal', '#009900')});
+  $("#blue").click(function(event) {selectColor(event, '#colorsModal', '#000099')});
+  $("#yellow").click(function(event) {selectColor(event, '#colorsModal', '#FFFF00')});
+  $("#orange").click(function(event) {selectColor(event, '#colorsModal', '#FF9900')});
+  $("#brown").click(function(event) {selectColor(event, '#colorsModal', '#802A2A')});
+  $("#purple").click(function(event) {selectColor(event, '#colorsModal', '#663399')});
+  $("#eraser").click(function(event) {selectColor(event, '#colorsModal', '#FFFFFF')});
 
   // Width
-  $("#small").click(function() {style.strokeWidth = 2});
-  $("#medium").click(function() {style.strokeWidth = 5});
-  $("#large").click(function() {style.strokeWidth = 10});
+  $("#small").click(function(event) {selectWidth(event, '#sizesModal', 2)});
+  $("#medium").click(function(event) {selectWidth(event, '#sizesModal', 5)});
+  $("#large").click(function(event) {selectWidth(event, '#sizesModal', 10)});
 
   //Undo
   $("#undo").click(function() { DrawGlobals.undo(); });
 
   //Clear
-  $("#clearScreen").click(function() { DrawGlobals.clearScreen(); });
+  $("#cancel-reset").click(function(event) { event.preventDefault(); $('#resetModal').foundation('reveal', 'close'); });
+  $("#accept-reset").click(function(event) { resetBoard(event, '#resetModal') });
+
+  //Save
+  $("#save-button").click(function(event) { event.preventDefault(); $('#saveModal').foundation('reveal', 'close'); });
 });
+
+function selectColor(event, modalId, color) {
+  event.preventDefault();
+  style.strokeColor = color
+  $(modalId).foundation('reveal', 'close');
+};
+
+function selectWidth(event, modalId, width) {
+  event.preventDefault();
+  style.strokeWidth = width
+  $(modalId).foundation('reveal', 'close');
+};
+
+function resetBoard(event, modalId) {
+  event.preventDefault();
+  DrawGlobals.clearScreen();
+  $(modalId).foundation('reveal', 'close');
+}
